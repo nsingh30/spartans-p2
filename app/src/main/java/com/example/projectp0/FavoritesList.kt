b6f0f278b7bf9a57dfa49dcbac033c8bd2a49c64
Elmer Orellana <elgu90@gmail.com>
2022-05-27 15:19:55 -0700

    add favorites activity

---
+++ app/src/main/java/com/example/projectp0/FavoritesList.kt
@@ -0,0 +1,11 @@
+package com.example.projectp0
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoritesList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites_list)
+    }
+}
--- app/src/main/java/com/example/projectp0/MainActivity.kt
+++ app/src/main/java/com/example/projectp0/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.projectp0
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val linkFavList: View = findViewById(R.id.fav_view)
+        linkFavList.setOnClickListener{
+            val intent = Intent(this, FavoritesList::class.java)
+            startActivity(intent)
+        }
     }
 }
