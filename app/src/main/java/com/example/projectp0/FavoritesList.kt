636d165b610b3827641054bd551953c035e1529f
Elmer Orellana <elgu90@gmail.com>
2022-05-28 01:08:41 -0700

    add new recipe form

--- app/src/main/java/com/example/projectp0/FavoritesList.kt
+++ app/src/main/java/com/example/projectp0/FavoritesList.kt
@@ -1,11 +1,21 @@
 package com.example.projectp0
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FavoritesList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorites_list)
+
+        val btnAdd : Button = findViewById(R.id.btn_add)
+        btnAdd.setOnClickListener(){
+            val intent = Intent(this, NewRecipeForm:: class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/projectp0/NewRecipeForm.kt
@@ -0,0 +1,13 @@
+package com.example.projectp0
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class NewRecipeForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_recipe_form)
+
+    }
+}
