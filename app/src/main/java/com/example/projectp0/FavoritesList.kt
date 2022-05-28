973f4ad49542de33611580628c54f2734cacd31e
Elmer Orellana <elgu90@gmail.com>
2022-05-28 13:11:18 -0700

    complete new recepe form

--- app/src/main/java/com/example/projectp0/FavoritesList.kt
+++ app/src/main/java/com/example/projectp0/FavoritesList.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class FavoritesList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorites_list)
 
-        val btnAdd : Button = findViewById(R.id.btn_add)
+        val btnAdd : FloatingActionButton = findViewById(R.id.btn_add)
         btnAdd.setOnClickListener(){
             val intent = Intent(this, NewRecipeForm:: class.java)
             startActivity(intent)
